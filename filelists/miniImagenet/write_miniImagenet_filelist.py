cf5ffda2058515baf573b03420cdc5d432129022
Giang T. Pham <48829690+giangTPham@users.noreply.github.com>
2022-03-13 20:34:49 +0700

    change link to data

--- filelists/miniImagenet/write_miniImagenet_filelist.py
+++ filelists/miniImagenet/write_miniImagenet_filelist.py
@@ -7,7 +7,7 @@ import random
 import re
 
 cwd = os.getcwd() 
-data_path = join(cwd,'ILSVRC2015/Data/CLS-LOC/train')
+data_path = join(cwd,'imagenet-mini/train')
 #data_path = join('/home/yuqing/phd/code/miniimagenet/images')
 savedir = './'
 dataset_list = ['base', 'val', 'novel']
